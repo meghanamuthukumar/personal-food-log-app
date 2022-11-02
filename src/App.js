9b42b5639d898c562ceeb7e149ded2040ad1fc2b
meghanamuthukumar <meghanamuthukumar14@gmail.com>
2022-11-02 12:06:08 -0400

    initial structure

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Personal Food Log App
+
+        <button>Experience {'>>'}
+        </button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
